511efccf3a4621251ca33c5bd86c438ffd0d5652
Jay Verkler <jlvgit@summit8.net>
2020-01-03 15:12:45 -0800

    raw dev
    raw dev

--- src/CrawlPrint.php
+++ src/CrawlPrint.php
@@ -3,15 +3,17 @@
 require 'MusicRequire.inc';
 
 // this is the log that will be made
-log_init("Print Log");
+log_init("Print");
 
 function crawlPrint($base_folder, $add_folder, $new_base_folder, $filename, $array_of_params)
 {
-  plog($filename . "\n\j");
+  plog($filename . "\r\n");
 }
 
 $empty = array();
 $startPoint = "C:\Quentin\MusicReference\Music";
+$startPoint = $homedir . "\MusicRef";
+
 crawl($startPoint, "", "", "crawlPrint", $empty);
 
 ?>
