c3d567cf7dd7e0047eaa164fa1e369298ebf5b35
Jay Verkler <jlvgit@summit8.net>
2021-08-22 00:01:46 -0700

    snap - in dev
    snap - in dev

--- test/test-log-basic.php
+++ test/test-log-basic.php
@@ -2,9 +2,12 @@
 
 require "MusicRequire.inc";
 
+
 print "Testing Log System\n\n";
 
 log_init("Test-Log", NULL);
+
+logp("info",array("test array 1", "test array 2", "test array 3"));
 
 logp("echo,exit2","Test log.");
 
