934475850de7e927206aafc19fb9864e158d5e1e
Jay Verkler <jlvgit@summit8.net>
2022-08-05 09:35:56 -0700

    cleaned up MultiDisk

--- src/MultiDisk.php
+++ src/MultiDisk.php
@@ -43,12 +43,6 @@ logp("echo,exit0","MultiDisk successfully merged directories.  See logs for deta
 exit;
 
 
-// Basic flow:
-//  Find files, check artist/album, load in cuefile
-//  Trackify fixup to load files in $wav and fix tracks on each cuefile
-//  Load each cuefile path into $trash for later processing
-//  Trackify again reorder without fixup once we have every disk loaded
-
 
 // function setupMultiMerge(&$cuefile, &$cue_meta, &$wav, &$trash, $options)
 //  $add_folder - the folder path to add to $base_folder in crawl function.
@@ -58,8 +52,14 @@ exit;
 //              array can be referced with same index. Tagged elements form
 //              second dimension of array, e.g. ['dir'],['artist'],...
 //  $wav - wav array (see definition of array in moveWAV)
-
-
+//  $options - array of options
+//
+// Basic flow:
+//  Find files, check artist/album, load into single cuefile, capturing some
+//    needed meta data.
+//  Process cuefile through all steps, which loads $wav and is ready to confirm
+//    and execute.  Note that $wav is used to store a runtime piece of data in
+//    tooLongExists
 
 function setupMultiMerge(&$cuefile, &$cue_meta, &$wav, &$trash, $options) {
   // globals from parameter
@@ -180,29 +180,18 @@ function setupMultiMerge(&$cuefile, &$cue_meta, &$wav, &$trash, $options) {
 
 
 // function confirmMerge($cuefile, $wav)
-//
-//  $add_folder - the folder path to add to $base_folder in crawl function.
-//                serves to capture artist/album
 //  $cuefile - cuefile array
-//  $cue_meta - cuefile meta data. Mirrors cuefile element for element so that
-//              array can be referced with same index. Tagged elements form
-//              second dimension of array, e.g. ['dir'],['artist'],...
 //  $wav - wav array (see definition of array in moveWAV)
 //
-// $finalDir - directory of combined multi-disk
-// $multiDisks - array of all dist add ones (i.e. Disc 1, (Disc 1), or nothing)
+// Returns TRUE on success
 //
-// intro function - takes in user input to confirm what is being renamed to what
-//                  NOTE - user must be in artist directory for system to work
-// returns 0 on failure
+// Confirms titles and track file changes with user. Exits if not confirmed.
 
 function confirmMerge($cuefile, $wav)  {
   global $finalDir;
   global $multiDisks;
 
   print "\n\n*** Confirming MultiDisk Details\n\n";
-
-  // diskfreespace
 
   print "Disc Parameters:\n";
   // checks that all cds actually exist
@@ -231,17 +220,25 @@ function confirmMerge($cuefile, $wav)  {
     print "Dir:  {$song["old_dir"]}\nSong: {$old_song}\n";
     print " --> '{$song["new"]}'\n\n";
   }
-//print_r($wav);
+
   // confirm
   if (strtoupper(readline("\nConfirm >")) != "Y") exit;
 
-
   return TRUE;
 
-}
+} // end of confim function
 
 
-
+// function executeMerge(&$cuefile, &$wav, &$trash, $options)
+//  $cuefile - cuefile array
+//  $cue_meta - cuefile meta data. Mirrors cuefile element for element so that
+//              array can be referced with same index. Tagged elements form
+//              second dimension of array, e.g. ['dir'],['artist'],...
+//  $wav - wav array (see definition of array in moveWAV)
+//  $options - array of options
+//
+// Returns TRUE on success, FALSE if errors
+// Executes merge set up by setup merge.
 
 function executeMerge(&$cuefile, &$wav, &$trash, $options)  {
   // globals from parameter
@@ -311,267 +308,10 @@ function executeMerge(&$cuefile, &$wav, &$trash, $options)  {
       logp("error","ERROR: could not clear a directory, '{$disc}'");
       $return = FALSE;
     }
-    
+
   return $return;
 } // end of executeMerge function
 
 
-
-// function intro($currentDir, $oldDir, $newDir, $trackExcerpt)
-// $finalDir - directory of combined multi-disk
-// $multiDisks - array of all dist add ones (i.e. Disc 1, (Disc 1), or nothing)
-//
-// intro function - takes in user input to confirm what is being renamed to what
-//                  NOTE - user must be in artist directory for system to work
-// returns 0 on failure
-function intro_DEPR(){
-  global $finalDir;
-  global $multiDisks;
-
-  // defines readline function if it doesn't exist
-  if(!function_exists("readline")) {
-    function readline($prompt = null){
-      if($prompt){
-          echo $prompt;
-      }
-      $fp = fopen("php://stdin","r");
-      $line = rtrim(fgets($fp, 1024));
-      return $line;
-    }
-  }
-
-  // checks that all cds actually exist
-  foreach($multiDisks as $disk){
-    if(!is_dir($disk)){
-      logp("error", "ERROR: given directory handle does not exist: {$disk}");
-      return 0;
-    }
-  }
-
-  // checks with user about $finalDir and $multiDisks
-  print "Is this finalDir: " . $finalDir . "? Y/N\n";
-  $response = readline();
-  if(strtoupper($response) == "Y"){
-
-    print "Are you sure? Y/N\n";
-    $response = readline();
-
-    if(strtoupper($response) == "Y"){
-
-      print "Are these the multiple disks? Y/N\n";
-      foreach($multiDisks as $disk){
-        print $disk . "\n";
-      }
-      $response = readline();
-
-      if(strtoupper($response) == "Y"){
-
-        print "Are you sure? Y/N\n";
-        $response = readline();
-
-        if(strtoupper($response) == "Y"){
-
-          print "Affirmative: Comencing combine\n";
-          combine($finalDir, $multiDisks);
-
-        }else if(strtoupper($response) == "N"){
-          print "Affirmative. Rename Aborted";
-          return 0;
-        }else{
-          print "Invalid input. Rename Aborted";
-          return 0;
-        }
-
-      }else if(strtoupper($response) == "N"){
-        print "Affirmative. Rename Aborted";
-        return 0;
-      }else{
-        print "Invalid input. Rename Aborted";
-        return 0;
-      }
-
-    }else if(strtoupper($response) == "N"){
-      print "Affirmative. Rename Aborted";
-      return 0;
-    }else{
-      print "Invalid input. Rename Aborted";
-      return 0;
-    }
-
-  }else if(strtoupper($response) == "N"){
-    print "Affirmative. Rename Aborted";
-    return 0;
-  }else{
-    print "Invalid input. Rename Aborted";
-    return 0;
-  }
-}
-
-
-
-// function combine($currentDir, $oldDir, $newDir, $trackExcerpt)
-// $finalDir - directory of combined multi-disk
-// $multiDisks - array of all dist add ones (i.e. Disc 1, (Disc 1), or nothing)
-//
-// combine function - actually combines the multiple disks into one directory as $finalDir
-//                    NOTE - all directories must be in new, correct format
-// returns 0 on failure
-function combine_DEPR($finalDir, $multiDisks){
-  // creates $finalDir if it doesn't exist
-  if(!is_dir($finalDir)){
-    mkdir($finalDir);
-  }
-  // all variables needed to combine $multiDisks
-  $newCue = array();
-  $trackNum = 1;
-  // uses foreach to go through $multiDisks and the .cue files. May need to change
-  foreach($multiDisks as $num => $disk){
-    $oldCue = file($disk . "/" . $disk . ".cue", FILE_IGNORE_NEW_LINES);
-    if($num != 0){
-      $cdNum = $num + 1;
-      array_push($newCue, "REM CD " . $cdNum);
-      // cuts off the beginning part of a cue file up to the first FILE line
-      $cutLength = 0;
-      while(!preg_match( '/^\a*FILE/', $oldCue[$cutLength] )){
-        $cutLength += 1;
-      }
-
-      array_splice($oldCue, 0, $cutLength);
-    }else{
-      if(!$changedAlbum && preg_match("/TITLE/", $oldCue[$i])){
-        $oldCue[$i] = "TITLE \"" . $disk . "\"";
-        $changedAlbum = true;
-      }
-    }
-
-    // $changedAlbum lets turns true when the first TITLE (aka title of Album) has been changed
-    $changedAlbum = false;
-    // puts together the track and changes .wav
-    for($i = 0; $i < count($oldCue); $i++){
-
-      if(preg_match ( '/^\a*FILE/', $oldCue[$i] ) === 1){
-        $isTwoDigit = true;
-        // $song is just the song title with no FILE
-        $song = preg_replace("/FILE \"/", '', $oldCue[$i]);
-        $song = preg_replace("/\" WAVE/", '', $song);
-
-        if(preg_match("/^\d\d\d /", $song)){
-          $isTwoDigit = false;
-        }
-
-        $newSong = "";
-        if($trackNum < 10 && $isTwoDigit){
-          // renaming the old .wav file to new .wav file in $finalDir
-          $newSong = preg_replace("/^\d\d/", "0{$trackNum}", $song);
-          // renames FILE line
-          $oldCue[$i] = preg_replace("/\"\d\d /", "\"0{$trackNum} ", $oldCue[$i]);
-          // changes the TRACK number
-          $oldCue[$i+1] = preg_replace("/\d\d /", "0{$trackNum} ", $oldCue[$i+1]);
-        }else if($trackNum >= 10 && $isTwoDigit){
-          // renaming the old .wav file to new .wav file in $finalDir
-          $newSong = preg_replace("/^\d\d/", "{$trackNum}", $song);
-          // renames FILE line
-          $oldCue[$i] = preg_replace("/\"\d\d /", "\"{$trackNum} ", $oldCue[$i]);
-          // changes the TRACK number
-          $oldCue[$i+1] = preg_replace("/\d\d /", "{$trackNum} ", $oldCue[$i+1]);
-        }else if($trackNum < 10 && !$isTwoDigit){
-          // renaming the old .wav file to new .wav file in $finalDir
-          $newSong = preg_replace("/^\d\d\d/", "00{$trackNum}", $song);
-          // renames FILE line
-          $oldCue[$i] = preg_replace("/\"\d\d\d /", "\"00{$trackNum} ", $oldCue[$i]);
-          // changes the TRACK number
-          $oldCue[$i+1] = preg_replace("/\d\d\d /", "00{$trackNum} ", $oldCue[$i+1]);
-        }else if($trackNum >= 10 && $trackNum < 100 && !$isTwoDigit){
-          // renaming the old .wav file to new .wav file in $finalDir
-          $newSong = preg_replace("/^\d\d\d/", "0{$trackNum}", $song);
-          // renames FILE line
-          $oldCue[$i] = preg_replace("/\"\d\d\d /", "\"0{$trackNum} ", $oldCue[$i]);
-          // changes the TRACK number
-          $oldCue[$i+1] = preg_replace("/\d\d\d /", "0{$trackNum} ", $oldCue[$i+1]);
-        }else if($trackNum >= 100 && !$isTwoDigit){
-          // renaming the old .wav file to new .wav file in $finalDir
-          $newSong = preg_replace("/^\d\d\d/", "{$trackNum}", $song);
-          // renames FILE line
-          $oldCue[$i] = preg_replace("/\"\d\d\d /", "\"{$trackNum} ", $oldCue[$i]);
-          // changes the TRACK number
-          $oldCue[$i+1] = preg_replace("/\d\d\d /", "{$trackNum} ", $oldCue[$i+1]);
-        }
-
-        if($newSong == ""){
-          logp("error", "ERROR: Failure on changing song title in {$disk}");
-          return 0;
-        }
-
-        if(isDryRun()){
-          logp("notify", "Would be renaming {$newSong}");
-        }else{
-          // actually moves the .wav file to $finalDir
-          $goodWav = rename($disk . "/" . $song, $finalDir . "/" . $newSong);
-          if(!$goodWav){
-            logp("error", "ERROR: Could not rename {$song} as {$newSong}");
-            return 0;
-          }
-        }
-
-        $trackNum++;
-      }
-
-    }
-
-    // now cue should be all good to add and discard $oldCue
-    $newCue = array_merge($newCue, $oldCue);
-    if(isDryRun()){
-      logp("notify", "Would be deleting {$disk}.cue");
-    }else{
-      unlink($disk . "/" . $disk . ".cue");
-    }
-
-    // must check if $disk = $finalDir because this will happen sometimes
-    if($disk != $finalDir){
-      // sees if $disk has any other files in it and moves them to $finalDir
-      $dir = opendir($disk);
-      while(($file = readdir($dir)) !== false){
-        if(getSuffix($file) === "cue" || getSuffix($file) === "wav"){
-          logp("error", "ERROR: Found {$file} still in {$disk} when it should have been moved/deleted");
-          return 0;
-        }else if($file != "." && $file != ".."){
-          if(isDryRun()){
-            logp("notify", "Would be renaming {$disk}/{$file} as {$disk}/{$file}-{$cdNum}.{$end}");
-          }else{
-            $cdNum = $num + 1;
-            $end = getSuffix($file);
-            $name = preg_replace("/\....$/", "", $file);
-            rename($disk . "/" . $file, $finalDir . "/" . $name . "-" . $cdNum . "." . $end);
-          }
-        }
-      }
-      closedir($dir);
-
-      if(isDryRun()){
-        logp("notify", "Would be deleting {$disk} folder");
-      }else{
-        // deletes $disk directory
-        unlink($disk);
-        logp("info", "{$disk} has been deleted. Moving onto next disk");
-      }
-    }
-    logp("info", "{$finalDir} has been compiled. Function combine completed");
-
-  }
-
-  // puts $newCue back into a .cue file and puts it in the $finalDir folder
-  addLineTerm($newCue);
-  if(isDryRun()){
-    logp("notify", "Would be making {$finalDir}.cue from $newCue");
-  }else{
-    file_put_contents($finalDir . "/" . $finalDir . ".cue", $newCue);
-  }
-}
-
-//
-// Main program
-//
-
-//intro();
 
 ?>
